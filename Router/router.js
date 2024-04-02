2490a79a5fbb480eea2d6755d5b02d1a1c7e872f
tuioe <qodlgks123@naver.com>
2024-04-02 06:46:21 +0000

    :recycle: Routing Server 리팩토링

--- Router/router.js
+++ Router/router.js
@@ -3,18 +3,30 @@ const url = require("url");
 
 http.createServer((req,res) => {
     const path = url.parse(req.url,true).pathname;
-    res.setHeader("Content-Type","text/html");
+    res.setHeader("Content-Type","text/html; charset=utf-8");
 
     if(path == "/user"){
-        res.end("[user] name: JS age: 0");
+        user(req,res);
     }else if(path == "/list"){
-        res.end(`<ul>
-        <li>리스트1</li>
-        <li>list1</li>
-        <li>list1</li>
-        </ul>`);
+        list(req,res);
     }else{
-        res.statusCode = 404;
-        res.end("404 page not found");
+        notFound(req,res);
     }
 }).listen("3000", () => console.log("라우터"));
+
+const user = (req,res) => {
+    res.end("[user] name: JS age: 0");
+};
+
+const list = (req,res) => {
+    res.end(`<ul>
+    <li>리스트1</li>
+    <li>list1</li>
+    <li>list1</li>
+    </ul>`);
+};
+
+const notFound = (req,res) => {
+    res.statusCode = 404;
+    res.end("404 page not found");
+};
